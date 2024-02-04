f28e2de4b91ad256abd598d3b387d9f699e5c26b
adrmichalik <adrmichalik@gmail.com>
2024-02-04 19:52:35 +0100

    Change div Weather Cards to component WeatherCard

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "bootstrap/dist/css/bootstrap.min.css";
 import Carousel from "react-bootstrap/Carousel";
+import WeatherCard from "./components/WeatherCard";
 
 function App() {
   const [index, setIndex] = useState(0);
@@ -24,10 +25,10 @@ function App() {
       controls={true}
     >
       <Carousel.Item>
-        <div className="weather_card">Weather Card #1</div>
+        <WeatherCard />
       </Carousel.Item>
       <Carousel.Item>
-        <div className="weather_card">Weather Card #2</div>
+        <WeatherCard />
       </Carousel.Item>
     </Carousel>
   );
