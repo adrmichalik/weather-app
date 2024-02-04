edc09aed3fb02c6a021e5f9eaad79de1079d9da1
adrmichalik <adrmichalik@gmail.com>
2024-02-04 20:13:40 +0100

    WeatherCard template
    WeatherCard contains all the features that will be included in the future.
    Those features are static text that will be generated based on provided informations.
    Provides also small cosmetic changes.

--- src/components/WeatherCard.jsx
+++ src/components/WeatherCard.jsx
@@ -1,7 +1,40 @@
 import React from "react";
+import { ListGroup, ListGroupItem } from "react-bootstrap";
+import Card from "react-bootstrap/Card";
 
 function WeatherCard() {
-  return <div className="weather_card">Weather Card placeholder</div>;
+  return (
+    <Card className="weather_card">
+      <Card.Body>
+        <div className="main_informations">
+          <h5>Location name</h5>
+          <h2>10 *C</h2>
+          <h4>Weather description</h4>
+          <span>From 0 *C to 20 *C</span>
+        </div>
+        <div>
+          {/* In the future HourlyTempSlider (might change name) */}
+          {/* It will contain temp, weather related icon and hour */}
+          <div className="slider">Slider Placeholder</div>
+
+          {/* In the future it will contain multiple forecast items*/}
+          {/* Maybe separate component */}
+          <ListGroup>
+            {/* Header */}
+            <ListGroup.Item variant="light">
+              Multiple day forecast
+            </ListGroup.Item>
+            {/* Items */}
+            <ListGroupItem>
+              <span>Day name</span>
+              <span>Weather Icon</span>
+              <span>Temp ranges</span>
+            </ListGroupItem>
+          </ListGroup>
+        </div>
+      </Card.Body>
+    </Card>
+  );
 }
 
 export default WeatherCard;
