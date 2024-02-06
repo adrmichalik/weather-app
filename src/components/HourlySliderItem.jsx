a8cd3aa951015e096537e6299042fa6c55ae5fb3
adrmichalik <adrmichalik@gmail.com>
2024-02-06 14:49:46 +0100

    Add new HourlySliderItem component
    It will contain hour, icon and temperature, and also temperature unit as passed as Props

---
+++ src/components/HourlySliderItem.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+function HourlySliderItem() {
+  return (
+    <div className="hourly_slider_item">
+      <div>Hourly</div>
+      <div>Slider</div>
+      <div>Item</div>
+    </div>
+  );
+}
+
+export default HourlySliderItem;
