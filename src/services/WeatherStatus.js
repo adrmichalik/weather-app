e8d16460b6166c94d19a6856051146392615af48
adrmichalik <adrmichalik@gmail.com>
2024-02-06 17:34:40 +0100

    Add service

---
+++ src/services/WeatherStatus.js
@@ -0,0 +1,8 @@
+// *****************************
+// WeatherStatus require code to determine resources
+//
+// WeatherStatus provides:
+// weather description such as "clear sky", "rainy", etc
+// weather icon that represents descrption
+//
+// *****************************
