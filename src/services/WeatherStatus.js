0ae3911aa8cd44286fba44cf1a072ba189c6a730
adrmichalik <adrmichalik@gmail.com>
2024-02-06 17:36:02 +0100

    Add WeatherInfo class

--- src/services/WeatherStatus.js
+++ src/services/WeatherStatus.js
@@ -6,3 +6,16 @@
 // weather icon that represents descrption
 //
 // *****************************
+
+// WeatherInfo
+// Object represents particular WeatherStatus based on code
+class WeatherInfo {
+  constructor(description, icon, icon_night) {
+    this.description = description;
+    this.icon = icon;
+
+    // With no night icon passed, we just use universal one
+    if (icon_night == undefined) this.icon_night = this.icon;
+    else this.icon_night = icon_night;
+  }
+}
