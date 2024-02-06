3ccef9423b4c278dc84ac24f2152b9cc4b18cc04
adrmichalik <adrmichalik@gmail.com>
2024-02-06 14:37:41 +0100

    Clean-up import non-existing files

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,4 @@
 import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
 import "bootstrap/dist/css/bootstrap.min.css";
 import Carousel from "react-bootstrap/Carousel";
 import WeatherCard from "./components/WeatherCard";
