40f0ca902acf6ac6b9ffc1c33c726de3fab81458
adrmichalik <adrmichalik@gmail.com>
2024-02-06 16:20:43 +0100

    Replace static placeholder texts with Prop based texts

--- src/components/HourlySliderItem.jsx
+++ src/components/HourlySliderItem.jsx
@@ -9,9 +9,15 @@ function HourlySliderItem({
 }) {
   return (
     <div className="hourly_slider_item">
-      <div>Hourly</div>
-      <div>Slider</div>
-      <div>Item</div>
+      <div>
+        {hour % 24}
+        {minutes != undefined && ":" + minutes}
+      </div>
+      <div>{icon}</div>
+      <div>
+        {temperature}
+        {temperature_unit}
+      </div>
     </div>
   );
 }
