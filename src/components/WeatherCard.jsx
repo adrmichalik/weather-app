d8841d222384246b05b0b246470167cb1b93e77a
adrmichalik <adrmichalik@gmail.com>
2024-02-06 15:24:56 +0100

    Add API's data fetch with new control states

--- src/components/WeatherCard.jsx
+++ src/components/WeatherCard.jsx
@@ -1,9 +1,37 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
+import axios from "axios";
 import ListGroup from "react-bootstrap/ListGroup";
 import Card from "react-bootstrap/Card";
 
 function WeatherCard({ locationName, coordinates }) {
   const [location_name, setLocation_name] = useState(locationName);
+  const [weather, setWeather] = useState();
+  const [loadingApiResponse, setLoadingApiResponse] = useState(true);
+  const [errorApiResponse, setErrorApiResponse] = useState(false);
+
+  useEffect(() => {
+    let { longitude, latitude } = coordinates;
+
+    const url = `https://api.open-meteo.com/v1/forecast?latitude=${latitude}&longitude=${longitude}&hourly=temperature_2m,weather_code&timezone=auto&current=temperature_2m,weather_code&daily=weather_code,temperature_2m_max,temperature_2m_min,sunrise,sunset`;
+
+    axios
+      .get(url)
+      .then((res) => {
+        setWeather(res.data);
+        setLoadingApiResponse(false);
+        // For development only
+        console.log(res.data);
+      })
+      .catch((error) => {
+        // For now only log it into console, in the future Alert it
+        setErrorApiResponse(true);
+        setLoadingApiResponse(false);
+        console.error(error);
+      });
+  }, []);
+
+  if (errorApiResponse) return <h1>Error</h1>;
+  if (loadingApiResponse) return <h1>Loading</h1>;
 
   return (
     <Card className="weather_card">
