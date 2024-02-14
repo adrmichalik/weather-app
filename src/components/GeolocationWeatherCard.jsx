c0334cc2defb788e2733c131cbcc31806248a5c7
adrmichalik <adrmichalik@gmail.com>
2024-02-14 13:17:34 +0100

    Add GeolocationWeatherCard component

---
+++ src/components/GeolocationWeatherCard.jsx
@@ -0,0 +1,43 @@
+import WeatherCard from "./WeatherCard";
+import useGeolocation from "../hooks/useGeolocation";
+import { useEffect, useState } from "react";
+import axios from "axios";
+
+function GeolocationWeatherCard({ handleError }) {
+  const [geoloactionCityName, setGeolocationCityName] = useState();
+  const [loadingGeolocation, errorGeolocation, coordinates] = useGeolocation();
+
+  useEffect(() => {
+    if (loadingGeolocation == true || errorGeolocation == true) return;
+
+    let { latitude, longitude } = coordinates;
+
+    // API to get actual position as town name
+    const geoLoactionUrl = `https://api.bigdatacloud.net/data/reverse-geocode-client?latitude=${latitude}&longitude=${longitude}&localityLanguage=en`;
+
+    axios
+      .get(geoLoactionUrl)
+      .then((res) => {
+        setGeolocationCityName(res.data.city);
+      })
+      .catch((error) => {
+        console.error(error);
+        setGeolocationCityName("--");
+        handleError();
+        // In the future alert
+      });
+  }, [coordinates]);
+
+  if (errorGeolocation) {
+    handleError();
+    return;
+  }
+  if (loadingGeolocation || geoloactionCityName == undefined)
+    return <h1>Loading</h1>;
+
+  return (
+    <WeatherCard locationName={geoloactionCityName} coordinates={coordinates} />
+  );
+}
+
+export default GeolocationWeatherCard;
