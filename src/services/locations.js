4f0d1ed1d32f2de4852493c43591f0e766048bbe
adrmichalik <adrmichalik@gmail.com>
2024-02-17 18:35:30 +0100

    Add locations service
    It provides function to load locations from localStorage and 2 functions to control data: adding and deleting.

---
+++ src/services/locations.js
@@ -0,0 +1,76 @@
+import { addToArray, add, loadArray } from "./localStorageServices";
+import { v4 as uuid } from "uuid";
+
+class Location {
+  constructor(locationName, latitude, longitude) {
+    this.id = uuid();
+    this.locationName = locationName;
+    this.coordinates = {
+      latitude: latitude,
+      longitude: longitude,
+    };
+  }
+}
+const SAVED_LOCATIONS_KEY = "locations";
+
+/**
+ * Loads `Locations` from localStorage
+ * - If `Locations` are not present in localStorage returns empty Array and sets empty Array in localStorage
+ * @returns {Array<Location>}
+ */
+//
+export function loadLocations() {
+  const locationsFromLocalStorage = loadItem(SAVED_LOCATIONS_KEY);
+
+  if (
+    locationsFromLocalStorage == null ||
+    locationsFromLocalStorage == undefined
+  ) {
+    // Locations item are not present in local storage, set it to default empty array
+    // For development
+    console.warn(
+      "Locations from local storage is null. Setting default value."
+    );
+    saveItem(SAVED_LOCATIONS_KEY, []); // Set default value in localStorage
+    return [];
+  }
+
+  return JSON.parse(locationsFromLocalStorage);
+}
+
+/**
+ * Adds new location to display weather card and returns updated array
+ * @param {string} locationName
+ * @param {number} latitude
+ * @param {number} longitude
+ * @returns {Array<Location>}
+ */
+//
+export function addLocation(locationName, latitude, longitude) {
+  const newLocationInfo = new Location(locationName, latitude, longitude);
+
+  const locationsToUpdate = addToArray(SAVED_LOCATIONS_KEY, newLocationInfo);
+
+  return locationsToUpdate;
+}
+
+/**
+ * Delete location based on id and returns updated array
+ * - If `locationId` is not valid throws "No such ID."
+ * @param {string} locationId
+ * @returns {Array<Location>}
+ */
+//
+export function deleteLocation(locationId) {
+  let locationsToUpdate = loadArray(SAVED_LOCATIONS_KEY);
+
+  let newLocations = locationsToUpdate.filter(
+    (location) => location.id != locationId
+  );
+
+  if (locationsToUpdate.length === newLocations.length) throw "No such ID.";
+
+  add(SAVED_LOCATIONS_KEY, newLocations);
+
+  return newLocations;
+}
