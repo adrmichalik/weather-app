c90950e229fbff5dc6ee888c23e13e4bd34fce45
adrmichalik <adrmichalik@gmail.com>
2024-02-24 18:53:15 +0100

    Add alert add context

--- src/hooks/AlertProvider.jsx
+++ src/hooks/AlertProvider.jsx
@@ -1,15 +1,24 @@
 import React, { useContext, useState } from "react";
 
 const AlertsContext = React.createContext([]);
+const AddAlertContext = React.createContext();
 
 export function useAlerts() {
   return useContext(AlertsContext);
+}
+
+export function addAlert() {
+  return useContext(AddAlertContext);
 }
 
 export function AlertProvider({ children }) {
   const [alerts, setAlerts] = useState([]);
 
   return (
-    <AlertsContext.Provider value={alerts}>{children}</AlertsContext.Provider>
+    <AlertsContext.Provider value={alerts}>
+      <AddAlertContext.Provider value={addAlert}>
+        {children}
+      </AddAlertContext.Provider>
+    </AlertsContext.Provider>
   );
 }
